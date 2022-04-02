6cff5456aea63ff1ca796b20994c03f58f6b6fdf
Gleb647 <71315071+Gleb647@users.noreply.github.com>
2022-04-02 12:52:37 +0300

    Add files via upload

---
+++ 2_laba/demo/src/main/java/com/example/demo/TriangleController/TriangleController.java
@@ -0,0 +1,62 @@
+package com.example.demo.TriangleController;
+import com.example.demo.Exceptions.Exceptions;
+import com.example.demo.MyLogger.MyLogger;
+import com.example.demo.Service.Service;
+import com.example.demo.Triangle.Triangle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+
+@Validated
+@RestController
+public class TriangleController {
+    public TriangleController(){}
+
+    @GetMapping("/volume/perimeter")
+    public ResponseEntity getPerimeter(@Valid @RequestParam(name = "first", required = false) @NotNull @Min(1) String firstSide, @RequestParam(name = "second", required = false) @NotNull @Min(1) String secondSide, @RequestParam(name = "third", required = false) @NotNull @Min(1) String thirdSide, HttpServletRequest httpServletRequest) {
+        MyLogger.info("GET from /volume/perimeter");
+        int first = Integer.parseInt(firstSide);
+        int second = Integer.parseInt(secondSide);
+        int third = Integer.parseInt(thirdSide);
+        if (!Triangle.canExist(first, second, third)){
+            MyLogger.error("Triangle doesn't exist");
+            return Exceptions.ResponseServerError(httpServletRequest, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        else{
+            Triangle tr = new Triangle(first, second, third);
+            return new ResponseEntity(Service.calculatePerimeter(tr), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/volume/square")
+    public ResponseEntity getSquare(@Valid @RequestParam(name = "first", required = false) @NotNull @Min(1) String firstSide, @RequestParam(name = "second", required = false) @NotNull @Min(1) String secondSide, @RequestParam(name = "third", required = false) @NotNull @Min(1) String thirdSide, HttpServletRequest httpServletRequest) {
+        MyLogger.info("GET from /volume/square");
+        int first = Integer.parseInt(firstSide);
+        int second = Integer.parseInt(secondSide);
+        int third = Integer.parseInt(thirdSide);
+        if (!Triangle.canExist(first, second, third)){
+            MyLogger.error("Triangle doesn't exist");
+            return Exceptions.ResponseServerError(httpServletRequest, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        else{
+            Triangle tr = new Triangle(first, second, third);
+            return new ResponseEntity(Service.calculateSquare(tr), HttpStatus.OK);
+        }
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest httpServletRequest){
+        MyLogger.error("Wrong request parameters provided");
+        return Exceptions.ResponseReqError(httpServletRequest, HttpStatus.BAD_REQUEST);
+    }
+}
